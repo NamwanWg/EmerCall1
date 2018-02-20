32661e499ca248f725915d94cfd7c11701f00deb
namwan <thitimawg@gmail.com>
2018-02-20 12:01:11 +0700

    Add Fragment Success

--- app/src/main/java/namwan/snru/ac/th/emercall/MainActivity.java
+++ app/src/main/java/namwan/snru/ac/th/emercall/MainActivity.java
@@ -3,11 +3,24 @@ package namwan.snru.ac.th.emercall;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import namwan.snru.ac.th.emercall.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment
+        if (savedInstanceState == null){
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentFragment, new MainFragment()).commit();
+
+        }
+
+
+    }   // Main Method
+
+}   // Main Class
